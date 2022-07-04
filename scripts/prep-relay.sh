33c30b350117f5c3d5aa329a55affe306a4620b2
Islam <islamkwnd@gmail.com>
2022-07-04 17:19:48 +0000

    build(local-setup): Fix gitpod.yml, start scripts

--- scripts/prep-relay.sh
+++ scripts/prep-relay.sh
@@ -6,15 +6,15 @@ function makeBins {
     cp ~/relay/polkadot/target/release/polkadot bins/
     docker build --pull --rm -f ".github/dockerfiles/Dockerfile.relay" -t chocnet/polkadot-debug "."
     rm -r bins
-    gp sync-done relay
 }
 # Setup files for local
 function buildSpec {
+    mkdir -p ch_spec
     RELAY_BIN=~/relay/polkadot/target/release/polkadot
 
     $RELAY_BIN build-spec  --chain rococo-local --disable-default-bootnode > ch_spec/rococo-local.json
     $RELAY_BIN build-spec  --chain ch_spec/rococo-local.json --disable-default-bootnode --raw > ch_spec/rococo-local-raw.json
-
+    gp sync-done relay
 }
 
 function main {
